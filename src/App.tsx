0a9f272538724072eb3e300b5fd85ec00be1d90a
gawandeabhishek <abhishekgawande1667@gmail.com>
2025-06-23 16:15:33 +0530

    fixed landing page issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Landing from "./pages/landing/page";
+import Landing from "./pages/landing/Index";
 
 const queryClient = new QueryClient();
 
